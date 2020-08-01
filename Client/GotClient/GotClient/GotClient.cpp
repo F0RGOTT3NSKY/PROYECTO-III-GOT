798863f28de1ca8e665fe45fc2bfa557f35fbf68
Adrian Gomez <adriangomez2099@gmail.com>
2020-08-01 00:08:47 -0600

    Got <help> almost done

--- Client/GotClient/GotClient/GotClient.cpp
+++ Client/GotClient/GotClient/GotClient.cpp
@@ -2,10 +2,60 @@
 //
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+void menu();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Bienvenido a GOT v0.1.\n";
+	std::cout << "Ingrese \"got <help>\" para mas informacion. \n> ";
+	int x = 1;
+	while (x==1)
+	{
+		menu();
+	}
+	return 0;
+}
+enum string_code {
+	help,
+	init,
+};
+
+string_code coversion(std::string command) {
+	if (command == "got <help>") return help;
+	if (command == "got init <name>") return init;
+}
+
+void menu() {
+	std::string command;
+	std::getline(std::cin, command);
+	//std::cout << "Command you entered: " << command <<"\n";
+	switch (coversion(command))
+	{
+	case help:
+		system("cls");
+		std::cout << "Para obtener mas informacion acerca de un comando especifico, escriba \"got <help> <comando>\" \n\n";
+		std::cout << "init"<<"\t\t" <<"Instancia un nuevo repositorio en el servidor.\n";
+		std::cout << "help"<<"\t\t"<<"Muestra en la consola informacion de lo que hace cada comando en GOT.\n";
+		std::cout << "add"<<"\t\t"<<"Permite agregar todos los archivos que no esten registrados o que tengan nuevos cambios al repositorio.\n";
+		std::cout << "commit"<<"\t\t"<<"Envia los archivos agregados y pendientes de commit al server.\n";
+		std::cout << "status"<<"\t\t"<<"Muestra cuales archivos han sido cambiados, agregados o eliminados de acuerdo al commit anterior.\n";
+		std::cout << "rollback"<<"\t"<<"Permite regresar un archivo en el tiempo a un commit especifico.\n";
+		std::cout << "reset"<<"\t\t"<<"Deshace cambios locales para un archivo y lo regresa al ultimo commit.\n";
+		std::cout << "sync" << "\t\t" << "Recupera los cambios para un archivo en el server y lo sincroniza con el archivo en el cliente.\n";
+		std::cout << "\nPara mas informacion acerca del proyecto, consulte en r3dp4r4d153.github.io/PROYECTO-III-GOT/ \n\n";
+		std::cout << "Sugerencia: Para ingresar un comando utilice \"got <comando>\"\n> ";
+		break;
+	case init:
+		std::cout << "init  \n";
+		break;
+	default:
+		std::cout << "default \n";
+		break;
+	}
+
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
