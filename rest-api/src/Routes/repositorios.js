37fa3735809d06d78e78e107ce9d737ef1e6b213
Marco <marco99eu@gmail.com>
2020-08-12 18:30:34 -0600

    Rutas get de la tabla repositorios y archivos

--- rest-api/src/Routes/repositorios.js
+++ rest-api/src/Routes/repositorios.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const mysqlConnection = require('../Database');
 
+router.get('/repositorios', (req, res) => {
+    mysqlConnection.query('SELECT * FROM repositorios', (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/archivos', (req, res) => {
+    mysqlConnection.query('SELECT * FROM archivos', (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+router.get('/repositorios/:id', (req, res) => {
+    
+});
 
 module.exports = router;
